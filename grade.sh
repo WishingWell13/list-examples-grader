a2500e7df80d85be0c570a9b1b660f347ba49c4c
WishingWell13 <andy.smithwick@gmail.com>
2023-03-10 23:23:52 -0800

    Fixed issues with java line also running the ListExamples file

--- grade.sh
+++ grade.sh
@@ -1,6 +1,7 @@
 CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+mkdir student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
@@ -23,6 +24,8 @@ cd student-submission
 cp ListExamples.java ../mixed
 
 cd ..
+rm -rf student-submission
+
 
 cp TestListExamples.java mixed
 
@@ -38,15 +41,20 @@ then
     cat javac-errors.txt
 else
     echo "Compile Success"
-    java -cp $CPATH org.junit.runner.JUnitCore TestListExamples ListExamples >results.txt
-    if [[ `grep -c "FAILURES!!! " results.txt` -eq 0 ]]
+    java -cp $CPATH org.junit.runner.JUnitCore TestListExamples >results.txt
+    # echo `grep -c "FAILURES!!!" results.txt`
+    # echo `grep "FAILURES!!!" results.txt`
+    
+
+    if [[ `grep -c "FAILURES!!!" results.txt` -ne 0 ]]
     then
         echo "Grade is not 100%"
         RESULT_LINE=`grep "Tests run:" results.txt`
         COUNT=${RESULT_LINE:25:1}
         TOTAL=${RESULT_LINE:11:1}
+        SCORE=$(( $TOTAL - $COUNT ))
         echo "Your Score is"
-        echo "| Score: $COUNT/$TOTAL |"
+        echo "| Score: $SCORE/$TOTAL |"
     else
         echo "Grade is 100%"
     fi
